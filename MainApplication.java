c0536fbd7e7ee27a428cc398f964bd1726c1b60e
Yasmin Inoue <yasmiminoue@gmail.com>
2025-09-28 13:55:38 -0300

    Padronização MainApplication.java

--- MainApplication.java
+++ MainApplication.java
@@ -1,7 +1,6 @@
 import javax.swing.SwingUtilities;
 
 public class MainApplication {
-
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             MainFrame mainFrame = new MainFrame();
